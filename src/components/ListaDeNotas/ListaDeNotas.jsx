29863c25340f9e248a13df41089c375b43e43f43
Oziel Souza <ozysouzaa@gmail.com>
2021-03-28 00:37:22 -0300

    Revert "Adicionando funcionalidade deletar"
    This reverts commit ef1df94d6f7dc969cfc44c9deeb24f8198e8dac4.

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -9,12 +9,7 @@ class ListaDeNotas extends Component {
         {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota 
-              indice={index}
-              apagarNota={this.props.apagarNota}
-              titulo={nota.titulo} 
-              texto={nota.texto}
-              />
+              <CardNota titulo={nota.titulo} texto={nota.texto}/>
             </li>
           );
         })}
